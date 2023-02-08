78f6b9f44c01c736c1fcb13def37026f5b9622d1
biswa418 <biswajeetsutar418@gmail.com>
2023-02-08 09:56:43 +0530

    Add router and controllers

---
+++ controllers/homeController.js
@@ -0,0 +1,3 @@
+module.exports.home = function (request, response) {
+    return response.end('<h1>Home is here</h1>');
+}
---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const router = express.Router();
+const homeController = require('../controllers/homeController');
+
+console.log('started routing');
+
+router.get('/', homeController.home);
+router.use('/users', require('./users'));
+
+module.exports = router;
