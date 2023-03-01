7eb3c34fc8236edda38bb2fb7e39b3ebbcad5526
biswa418 <biswajeetsutar418@gmail.com>
2023-03-02 02:07:27 +0530

    Add comments and load

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -8,12 +8,20 @@ module.exports.home = function (request, response) {
     // return response.end('<h1>Home is here</h1>');
 
     //populating the users on each post
-    Post.find({}).populate('user').exec(function (err, posts) {
-        if (err) { console.log('Could not pop posts'); return; }
+    Post.find({})
+        .populate('user')
+        .populate({    // have to populate comment as well as who commented
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
+        })
+        .exec(function (err, posts) {
+            if (err) { console.log('Could not pop posts'); return; }
 
-        return response.render('home', {
-            title: "Codeial | Home",
-            posts: posts
-        });
-    })
+            return response.render('home', {
+                title: "Codeial | Home",
+                posts: posts
+            });
+        })
 }
