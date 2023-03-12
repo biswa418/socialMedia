7c5b9ba1c5281a307f90c59e8874473576a3c6d5
biswa418 <biswajeetsutar418@gmail.com>
2023-03-12 16:52:26 +0530

    send HTML mail

--- mailer/comments_mailer.js
+++ mailer/comments_mailer.js
@@ -2,12 +2,13 @@ const nodeMailer = require('../config/nodemailer');
 
 exports.newComment = (comment) => {
     console.log('Inside new comment mailer');
+    let htmlString = nodeMailer.renderTemplate({ comment: comment }, '/comments/new_comment.ejs');
 
     nodeMailer.transporter.sendMail({
         from: 'example@gmail.com', //sender email
         to: comment.user.email,
         subject: "New comment added to your post on Codeial",
-        html: "<h1> Hello World, Comments added </h1>"
+        html: htmlString
     },
         (err, info) => {
             if (err) {
